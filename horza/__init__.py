daf079300982f0dd175534741084a50177a869d5
Yauhen Shulitski <jsnjack@gmail.com>
2016-05-22 22:48:25 +0200

    Created working folder

--- horza/__init__.py
+++ horza/__init__.py
@@ -9,6 +9,11 @@ from settings import Settings
 
 settings = Settings
 
+# Make sure that working folder is exists
+if not os.path.exists(settings.db_path):
+    os.makedirs(settings.db_path)
+
+# Database section
 engine = create_engine(settings.db_url, echo=settings.db_echo)
 _Session = sessionmaker(bind=engine)
 session = _Session()
--- horza/settings.py
+++ horza/settings.py
@@ -1,3 +1,7 @@
+import os
+
+
 class Settings(object):
-    db_url = "sqlite:///horza.db"
+    db_path = os.path.join(os.path.expanduser("~"), ".horza")
+    db_url = "sqlite:///%s/horza.sqlite3" % db_path
     db_echo = True
